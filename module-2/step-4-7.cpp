b6002aab94f4338c1c7fb6b11418debc4f9583f8
Shahane Makiian <makiyansh@gmail.com>
2020-08-03 15:40:43 +0000

    Added strlen function

--- module-2/step-4-7.cpp
+++ module-2/step-4-7.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 
-void strcat(char* to, const char* from)
-{
-    while (*to) {
-        to++;
-    }
-    while (*from) {
-        *to++ = *from++;
-    }
+unsigned strlen(const char* str) {
+    int length = 0;
+    while (*str++) 
+        ++length;
+    return length;
 }
 
-int main()
-{
-    char str1[] = "first";
+void strcat(char* to, const char* from) {
+    to += strlen(to);
+    while ((*to++ = *from++));
+}
+
+int main() {
+    char str1[13] = "first";
     const char str2[] = "second";
-    strcat(&str1[0], &str2[0]);
+    strcat(str1, str2);
     std::cout << str1 << std::endl;
     return 0;
 }
